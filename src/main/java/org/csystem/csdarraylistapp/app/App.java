b72bfba8246a6e0724b80c7661eb9ff7c398b5e6
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2019-01-15 10:32:19 +0300

    002-ArrayNeyineYetmiyor?

--- src/main/java/org/csystem/csdarraylistapp/app/App.java
+++ src/main/java/org/csystem/csdarraylistapp/app/App.java
@@ -3,6 +3,19 @@ package org.csystem.csdarraylistapp.app;
 public class App {
     public static void main(String[] args)
     {
-        System.out.println("ArrayList sınıfını yazacak olsanız?");
+        System.out.println("Array'neyine yetmiyor?");
+
+        //öncelikle my-array alalım, işlemleri bu array üzerinden düşünelim
+        int [] a = {4, 8, 3, 1, 18, 9, 21, 20, 5, 17};
+
+        //bir static metot yazalım ekrana bassın -> advanced for kullanalım
+        printMyArray(a);
+    }
+
+    private static void printMyArray(int[] a)
+    {
+        for (int val : a)
+            System.out.print(val + " ");
+        System.out.println();
     }
 }
